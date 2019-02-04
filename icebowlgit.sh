93f461c16d4a6a211a177600ab69d628c348b0ed
BCooper58 <brc51101@gmail.com>
2019-02-04 08:46:11 -0800

    kek

---
+++ icebowlgit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  tedious;
+git config --global user.name  tedious;
+git config --global user.user tedious;
+git config --global user.email brc51101@gmail.com;
+git config --list;
